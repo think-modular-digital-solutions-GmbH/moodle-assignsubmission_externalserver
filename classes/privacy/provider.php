2df0190d0c8d5178edefcff172e46201f1531184
stefan weber <stefan.weber@think-modular.com>
2025-07-29 11:59:30 +0200

    Add privacy null provider

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -27,13 +27,6 @@ namespace assignsubmission_external_server\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->dirroot . '/mod/assign/locallib.php');
-
-use \core_privacy\local\metadata\collection;
-use \core_privacy\local\request\writer;
-use \core_privacy\local\request\contextlist;
-use \mod_assign\privacy\assign_plugin_request_data;
-
 /**
  * Privacy class for requesting user data.
  *
@@ -42,129 +35,9 @@ use \mod_assign\privacy\assign_plugin_request_data;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-        \core_privacy\local\metadata\provider,
-        \mod_assign\privacy\assignsubmission_provider,
-        \mod_assign\privacy\assignsubmission_user_provider {
+    \core_privacy\local\metadata\null_provider {
 
-    /**
-     * This is covered by mod_assign provider and the query on assign_submissions.
-     *
-     * @param  int $userid The user ID that we are finding contexts for.
-     * @param  contextlist $contextlist A context list to add sql and params to for contexts.
-     */
-    public static function get_context_for_userid_within_submission(int $userid, contextlist $contextlist) {
-        // This is already fetched from mod_assign.
-    }
-
-    /**
-     * This is also covered by the mod_assign provider and it's queries.
-     *
-     * @param  \mod_assign\privacy\useridlist $useridlist An object for obtaining user IDs of students.
-     */
-    public static function get_student_user_ids(\mod_assign\privacy\useridlist $useridlist) {
-        // No need.
-    }
-
-    /**
-     * If you have tables that contain userids and you can generate entries in your tables without creating an
-     * entry in the assign_submission table then please fill in this method.
-     *
-     * @param  userlist $userlist The userlist object
-     */
-    public static function get_userids_from_context(\core_privacy\local\request\userlist $userlist) {
-        // Not required.
-    }
-
-    /**
-     * Export all user data for this plugin.
-     *
-     * @param  assign_plugin_request_data $exportdata Data used to determine which context and user to export and other useful
-     * information to help with exporting.
-     */
-    public static function export_submission_user_data(assign_plugin_request_data $exportdata) {
-        // We currently don't show submissions to teachers when exporting their data.
-        $context = $exportdata->get_context();
-        if ($exportdata->get_user() != null) {
-            return null;
-        }
-        $user = new \stdClass();
-        $assign = $exportdata->get_assign();
-        $plugin = $assign->get_plugin_by_type('assignsubmission', 'file');
-        $files = $plugin->get_files($exportdata->get_pluginobject(), $user);
-        foreach ($files as $file) {
-            $userid = $exportdata->get_pluginobject()->userid;
-            writer::with_context($exportdata->get_context())->export_file($exportdata->get_subcontext(), $file);
-
-            // Plagiarism data.
-            $coursecontext = $context->get_course_context();
-            \core_plagiarism\privacy\provider::export_plagiarism_user_data($userid, $context, $exportdata->get_subcontext(), [
-                'cmid' => $context->instanceid,
-                'course' => $coursecontext->instanceid,
-                'userid' => $userid,
-                'file' => $file
-            ]);
-        }
-    }
-
-    /**
-     * Any call to this method should delete all user data for the context defined in the deletion_criteria.
-     *
-     * @param  assign_plugin_request_data $requestdata Information useful for deleting user data.
-     */
-    public static function delete_submission_for_context(assign_plugin_request_data $requestdata) {
-        global $DB;
-
-        \core_plagiarism\privacy\provider::delete_plagiarism_for_context($requestdata->get_context());
-
-        $fs = get_file_storage();
-        $fs->delete_area_files($requestdata->get_context()->id, 'assignsubmission_external_server', ASSIGNSUBMISSION_FILE_FILEAREA);
-
-        // Delete records from assignsubmission_external_server table.
-        $DB->delete_records('assignsubmission_external_server', ['assignment' => $requestdata->get_assign()->get_instance()->id]);
-    }
-
-    /**
-     * A call to this method should delete user data (where practical) using the userid and submission.
-     *
-     * @param  assign_plugin_request_data $deletedata Details about the user and context to focus the deletion.
-     */
-    public static function delete_submission_for_userid(assign_plugin_request_data $deletedata) {
-        global $DB;
-
-        \core_plagiarism\privacy\provider::delete_plagiarism_for_user($deletedata->get_user()->id, $deletedata->get_context());
-
-        $submissionid = $deletedata->get_pluginobject()->id;
-
-        $fs = get_file_storage();
-        $fs->delete_area_files($deletedata->get_context()->id, 'assignsubmission_external_server', ASSIGNSUBMISSION_FILE_FILEAREA,
-                $submissionid);
-
-        $DB->delete_records('assignsubmission_external_server', ['assignment' => $deletedata->get_assignid(), 'submission' => $submissionid]);
-    }
-
-    /**
-     * Deletes all submissions for the submission ids / userids provided in a context.
-     * assign_plugin_request_data contains:
-     * - context
-     * - assign object
-     * - submission ids (pluginids)
-     * - user ids
-     * @param  assign_plugin_request_data $deletedata A class that contains the relevant information required for deletion.
-     */
-    public static function delete_submissions(assign_plugin_request_data $deletedata) {
-        global $DB;
-
-        \core_plagiarism\privacy\provider::delete_plagiarism_for_users($deletedata->get_userids(), $deletedata->get_context());
-
-        if (empty($deletedata->get_submissionids())) {
-            return;
-        }
-        $fs = get_file_storage();
-        list($sql, $params) = $DB->get_in_or_equal($deletedata->get_submissionids(), SQL_PARAMS_NAMED);
-        $fs->delete_area_files_select($deletedata->get_context()->id, 'assignsubmission_external_server', ASSIGNSUBMISSION_FILE_FILEAREA,
-                $sql, $params);
-
-        $params['assignid'] = $deletedata->get_assignid();
-        $DB->delete_records_select('assignsubmission_external_server', "assignment = :assignid AND submission $sql", $params);
+    public static function get_reason(): string {
+        return 'privacy:metadata';
     }
 }
