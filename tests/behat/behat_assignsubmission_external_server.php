d824380acfd71d6ce3edfdc413ccf1ed60cb93ba
stefan weber <stefan.weber@think-modular.com>
2025-09-16 09:39:04 +0200

    Run demo server separately in behat

--- tests/behat/behat_assignsubmission_external_server.php
+++ tests/behat/behat_assignsubmission_external_server.php
@@ -42,8 +42,8 @@ class behat_assignsubmission_external_server extends behat_base {
      */
     public function i_add_external_server_pointing_to_this_site(): void {
         global $CFG, $DB;
-        $url = "{$CFG->wwwroot}/mod/assign/submission/external_server/tests/demo/moodle_external_assignment.php";
-        $formurl = "{$CFG->wwwroot}/mod/assign/submission/external_server/tests/demo/moodle_external_assignment_upload.php";
+        $url = "http://demo-server:80/mod/assign/submission/external_server/tests/demo/moodle_external_assignment.php";
+        $formurl = "http://demo-server:80/mod/assign/submission/external_server/tests/demo/moodle_external_assignment_upload.php";
         $record = (object)[
             'name' => 'behat_test',
             'url' => $url,
