c160533451e5503ea054913ee81efa74b5c7c2c9
stefan weber <stefan.weber@think-modular.com>
2025-09-16 10:52:26 +0200

    Fix URLs in behat step

--- tests/behat/behat_assignsubmission_external_server.php
+++ tests/behat/behat_assignsubmission_external_server.php
@@ -42,10 +42,18 @@ class behat_assignsubmission_external_server extends behat_base {
      */
     public function i_add_external_server_pointing_to_this_site(): void {
         global $CFG, $DB;
-        $base = getenv('DEMO_SERVER_BASE') ?: $CFG->wwwroot;
 
-        $url = $base . '/mod/assign/submission/external_server/tests/demo/moodle_external_assignment.php';
-        $formurl = $base . '/mod/assign/submission/external_server/tests/demo/moodle_external_assignment_upload.php';
+        if (getenv('DEMO_SERVER_BASE')) {
+
+            // We’re serving tests/demo as root in CI.
+            $url = $base . '/moodle_external_assignment.php';
+            $formurl = $base . '/moodle_external_assignment_upload.php';
+
+        } else {
+            // Local dev fallback – files under normal plugin path.
+            $url = $base . '/mod/assign/submission/external_server/tests/demo/moodle_external_assignment.php';
+            $formurl = $base . '/mod/assign/submission/external_server/tests/demo/moodle_external_assignment_upload.php';
+        }
 
         $record = (object)[
             'name' => 'behat_test',
