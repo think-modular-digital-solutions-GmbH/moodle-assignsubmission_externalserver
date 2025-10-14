757afb8db9dcb4c3f3a987e43f46947e8d2327da
stefan weber <stefan.weber@think-modular.com>
2025-10-14 14:55:27 +0200

    Fix version.php

--- version.php
+++ version.php
@@ -32,7 +32,7 @@ $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [];
 
 // Version.
-$plugin->version = 20250190901;
+$plugin->version = 2025101401;
 $plugin->release = '1.0.0';
 
 // Supported versions.
